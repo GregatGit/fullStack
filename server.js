d3bd18be2ee5d6a90e49ec62ace536aedf41e183
Greg Duncan <gregdd@outlook.com>
2017-03-03 17:39:47 +1000

    chapter 5

---
+++ server.js
@@ -0,0 +1,17 @@
+import config from './config';
+
+import express from 'express';
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Hello Express');
+});
+
+server.get('/about.html', (req, res) => {
+    res.send('the about page');
+});
+
+
+server.listen(config.port, () => {
+    console.info('Express listening on port ', config.port);
+});
